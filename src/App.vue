97907357f454d3098f56444d6ae5d21ee45649f5
peekpi <894646171@qq.com>
2021-09-25 22:08:45 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,222 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>1. install metamask and switch to Rinkeby network</div>
+  <div>2. click button to connect wallet</div>
+  <div>3. click button to init contract</div>
+  <div>4. click button to PlaceOrder</div>
+  <div>5. click button to AddDelivery</div>
+  <div>6. click button to CompleteOrder</div>
+  <div>...</div>
+  <button @click="connectWallet">connect to metamask</button>
+  <div>
+    chainId: {{ chainId }}
+    <br />
+    accounts:{{ accounts }}
+  </div>
+  <button @click="initContracts">InitContracts</button>
+  <ul>
+    <li v-for="(address, name, index) in contracts" :key="index">
+      {{ name }}:{{ address }}
+    </li>
+  </ul>
+  <button @click="PlaceOrder">PlaceOrder</button>
+  <button @click="AddDelivery">AddDelivery</button>
+  <button @click="CompleteOrder">CompleteOrder</button>
+  <ul>
+    <li>orderId: <input v-model="orderId" /></li>
+    <li>status: {{ orderStatus }}</li>
+    <li>derivativeContract:<input v-model="derivativeContract" /></li>
+    <li>derivativeTokenId: <input v-model="derivativeTokenId" /></li>
+    <li>licenseId: <input v-model="licenseId" /></li>
+  </ul>
+  <button @click="derivativeInfo">derivative token detail</button>
+  <ul>
+    <li v-for="(address, name, index) in derivativeDetial" :key="index">
+      {{ name }}:{{ address }}
+    </li>
+  </ul>
+  <button @click="orderInfo">orderDetail</button>
+  <ul>
+    <li v-for="(address, name, index) in orderDetail" :key="index">
+      {{ name }}:{{ address }}
+    </li>
+  </ul>
+  <button @click="licenseInfo">licenseDetail</button>
+  <ul>
+    <li v-for="(address, name, index) in licenseDetail" :key="index">
+      {{ name }}:{{ address }}
+    </li>
+  </ul>
+  <button @click="getAllIP">get IP list</button>
+  <ul>
+    <li v-for="(item, index) in ipList" :key="index">
+      token: {{ item.token }} tokenId: {{ item.tokenId }}
+    </li>
+  </ul>
+  <button @click="getAllOrders">get orders list</button>
+  <ul>
+    <li v-for="(order, index) in orderList" :key="index">
+      <span>----------------orderId:{{ index }}------------------</span>
+      <ul>
+        <li v-for="(value, key, index) in order" :key="index">
+          {{ key }}:{{ key == "licenseId" ? value.toHexString() : value }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <button @click="getAllServices">get services list</button>
+  <ul>
+    <li v-for="(service, index) in servicesList" :key="index">
+      <span>----------------serviceId:{{ index }}------------------</span>
+      <ul>
+        <li v-for="(value, key, index) in service" :key="index">
+          {{ key }}:{{ value }}
+        </li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { connect } from "./lib/metamask";
+import { InitContracts, AttachDerivative } from "./lib/contracts";
 
+const ArrayToObj = (arrayObj) => {
+  window.x = arrayObj;
+  return Object.keys(arrayObj)
+    .filter((key) => isNaN(Number(key)))
+    .reduce((obj, key) => ((obj[key] = arrayObj[key]), obj), {});
+};
+let contracts = {};
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      chainId: "",
+      accounts: [],
+      contracts: {},
+      orderId: "",
+      derivativeContract: "",
+      derivativeTokenId: "",
+      licenseId: "",
+      orderStatus: "",
+      derivativeDetial: {},
+      orderDetail: {},
+      licenseDetail: {},
+      ipList: [],
+      orderList: [],
+      servicesList: [],
+    };
+  },
+  methods: {
+    async connectWallet() {
+      const { chainId, accounts } = await connect();
+      this.chainId = chainId;
+      this.accounts = accounts;
+    },
+    initContracts() {
+      contracts = InitContracts();
+      Object.keys(contracts).forEach((name) => {
+        console.log(contracts[name].address);
+        this.contracts[name] = contracts[name].address;
+      });
+    },
+    async PlaceOrder() {
+      const DerivativeFactory = contracts.DerivativeFactory;
+      const originalNFT = contracts.MockNFT.address;
+      const originalTokenId = 1234; // this token already staked in the IP pool
+      const serviceId = 0; // this service already registered
+      const tx = await DerivativeFactory.place_order(
+        originalNFT,
+        originalTokenId,
+        serviceId
+      ); // wait tx send to network
+      const receipt = await tx.wait(1); // get receipt of tx, this mean tx is included in a block.
+      // parse log and find PlaceOrder to get orderId;
+      const filterTopic = DerivativeFactory.filters.PlaceOrder().topics[0];
+      const log = receipt.logs.find((log) => log.topics[0] == filterTopic);
+      const orderEvent = DerivativeFactory.interface.parseLog({
+        data: log.data,
+        topics: log.topics,
+      });
+      console.log("orderEvent args:", orderEvent.args);
+      this.orderId = orderEvent.args.orderId;
+      this.orderStatus = "Pending";
+    },
+    async AddDelivery() {
+      const orderId = this.orderId;
+      const DerivativeFactory = contracts.DerivativeFactory;
+      const tokenURI = "https://ipfs/xxxx";
+      const tx = await DerivativeFactory.add_delivery(orderId, tokenURI); // wait tx send to network
+      const receipt = await tx.wait(1); // get receipt of tx, this mean tx is included in a block.
+      // parse log and find PlaceOrder to get orderId;
+      const filterTopic = DerivativeFactory.filters.AddDelivery().topics[0];
+      const log = receipt.logs.find((log) => log.topics[0] == filterTopic);
+      const deliveryEvent = DerivativeFactory.interface.parseLog({
+        data: log.data,
+        topics: log.topics,
+      });
+      console.log("deliveryEvent args:", deliveryEvent.args);
+      this.derivativeContract = deliveryEvent.args.derivativeContract;
+      this.derivativeTokenId = deliveryEvent.args.derivativeTokenId;
+      this.orderStatus = "Deliveried";
+    },
+    async CompleteOrder() {
+      const orderId = this.orderId;
+      const DerivativeFactory = contracts.DerivativeFactory;
+      const tx = await DerivativeFactory.complete_order(orderId); // wait tx send to network
+      const receipt = await tx.wait(1); // get receipt of tx, this mean tx is included in a block.
+      // parse log and find PlaceOrder to get orderId;
+      const filterTopic = DerivativeFactory.filters.CompleteOrder().topics[0];
+      const log = receipt.logs.find((log) => log.topics[0] == filterTopic);
+      const completeEvent = DerivativeFactory.interface.parseLog({
+        data: log.data,
+        topics: log.topics,
+      });
+      console.log("completeEvent args:", completeEvent.args);
+      this.licenseId = completeEvent.args.licenseId.toHexString();
+      this.orderStatus = "Completed";
+    },
+
+    async derivativeInfo() {
+      const Derivative = AttachDerivative(this.derivativeContract);
+      const owner = await Derivative.ownerOf(this.derivativeTokenId);
+      const tokenURI = await Derivative.tokenURI(this.derivativeTokenId);
+      this.derivativeDetial = {
+        derivativeContract: this.derivativeContract,
+        tokenId: this.derivativeTokenId,
+        owner,
+        tokenURI,
+      };
+    },
+    async orderInfo() {
+      const DerivativeFactory = contracts.DerivativeFactory;
+      this.orderDetail = await DerivativeFactory.orders(this.orderId).then(
+        ArrayToObj
+      );
+    },
+    async licenseInfo() {
+      const Licenser = contracts.Licenser;
+      this.licenseDetail = await Licenser.get(this.licenseId).then(ArrayToObj);
+    },
+    async getAllIP() {
+      const IPPool = contracts.IPPool;
+      this.ipList = await IPPool.get_items();
+    },
+    async getAllOrders() {
+      const DerivativeFactory = contracts.DerivativeFactory;
+      this.orderList = await DerivativeFactory.get_orders().then((orders) =>
+        orders.map(ArrayToObj)
+      );
+    },
+    async getAllServices() {
+      const DerivativeFactory = contracts.DerivativeFactory;
+      this.servicesList = await DerivativeFactory.list_service().then(
+        (services) => services.map(ArrayToObj)
+      );
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +224,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+  list-style-type: none;
+}
+input {
+  width: 40em;
 }
 </style>
